1177335336dce5dc01ee00d0480cc47d549dacc7
Helle Børset Eidissen <helleeidissen@gmail.com>
2018-10-29 10:06:31 +0100

    added arbitrator

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # plab_robot
 PLAB-2 (TDT-4113) Project 6: Behavior-Based Robot Control
+
+
---
+++ arbitrator.py
@@ -0,0 +1,11 @@
+class Arbitrator:
+
+    def choose_action(self):
+        #go through all activ behaviours and choose winner
+        #choose deterministically or stochastically - our option
+        #return a tuple
+            #motor recommandations (one per motob) of the winning behaviour
+            #halt flag: should robot halt immediately?
+        pass
+
+
