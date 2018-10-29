c5e0167062e4b091dda1b17f19fed8f87b11db56
Helle Børset Eidissen <helleeidissen@gmail.com>
2018-10-29 09:43:47 +0100

    added sensob class

---
+++ sensob.py
@@ -0,0 +1,30 @@
+'''
+One Sensob for each of these sensors:
+    Ultrasonic sensor - one larger sensor
+    Infrared sensors - array of 6 small sensors on robot’s underside.
+    Camera - read extra document on PIL.
+'''
+
+
+class Sensob:
+
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def get_value(self):
+        pass
+
+    def reset(self):
+        pass
+
+
+    '''
+    Vet ikke hvor dette skal inn
+    Zumo Button
+        A very simple sensor.
+        The only method: wait_for_press
+    
+    '''
