353293184e7478bcff3e3f0334b5a006d990d96e
Helle Børset Eidissen <helleeidissen@gmail.com>
2018-10-30 12:40:56 +0100

    made arbitrator function

--- arbitrator.py
+++ arbitrator.py
@@ -6,7 +6,12 @@ class Arbitrator:
         #return a tuple
             #motor recommandations (one per motob) of the winning behaviour
             #halt flag: should robot halt immediately?
+        setting = None
+        haltflag = False
+        current_weight = -1
+        for behaviour in behaviourlist:
+            if behaviour.weight > current_weight:
+                current_weight = behaviour.weight
+                haltflag = behaviour.halt_request
+                setting = behaviour.motor_recommendations
         return setting, haltflag
-        pass
-
-
