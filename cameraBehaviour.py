b9573e27239b358221b0bca5b06a5409f51a6e99
Tora Seim Gunstad <toragunstad@gmail.com>
2018-10-31 11:30:01 +0100

    added is_black and camera behaviour

--- cameraBehaviour.py
+++ cameraBehaviour.py
@@ -1,33 +1,52 @@
 from behaviour import Behaviour
 from camera import Camera
 import imager2 as IMR
+from sensob import *
+
 
 class cameraBehaviour(Behaviour):
 
-    def __init__(self, bbcon=None, sensob=[], recommendations=[], priority=0.5):
-        Behaviour.__init__(bbcon, sensob, recommendations, priority)
+    def __init__(self, bbcon=None,  recommendations=[]):
+        Behaviour.__init__(bbcon, recommendations)
         self.name = "take picture"
-        self.can_take_picture = False
+        self.sensobs.append(CameraSensob)
+        self.sensobs.append(UltrasonicSensob)
+        self.priority = 0.2
 
     def consider_activation(self):
-        if self.can_take_picture: #fix IF-statement
+        if self.can_take_picture(): #fix IF-statement
             self.bbcon.activate_behaviour(self)
             self.active_flag = True
 
     def consider_deactivation(self):
-        if not self.can_take_picture: #fix IF-statement
+        if not self.can_take_picture(): #fix IF-statement
             self.bbcon.deactivate_behaviour(self)
             self.active_flag = False
         pass
 
-    def sense_and_act(self):
-        if self.can_take_picture:
-            print("Taking picture now!!")
-            img = IMR.Imager() #tar et bilde her??
-            img.dump_image('/') #aner ikke hva denne gjor?
-            self.motor_recommendations = [None] #send en motor rec vi onsker a utfore nar bildet er tatt
+    def can_take_picture(self):
 
-        self.priority = 0.9
+        distance = self.sensobs[1].get_value()
+        print(distance)
+
+        if (distance < 10 and distance > 0):
+            return True
+
+
+    def sense_and_act(self):
+        if self.can_take_picture():
+            print("Taking picture now!!")
+            img = Imager(image=self.sensobs[0].get_value(), mode='RGB')
+            img.dump_image('test.jpeg')
+
+            if img.is_black():
+                print("picture is black")
+                self.motor_recommendations = ['b', 3] #kjorer bakover i 3 sec
+                self.priority = 0.9
+            else:
+                print("picture is bright")
+                self.motor_recommendations = ['f', 2] #kjorer forover i 2 sec
+                self.priority = 0.2
 
 
 
